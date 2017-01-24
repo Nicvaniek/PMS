fe33d30a9a82cbe5e214bd7467c94761b7023c84
Nicolai van Niekerk <nicvaniek@gmail.com>
2017-01-24 11:00:51 +0200

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,20 @@
+(function($) {
+    $(function() {
+
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+        $('select').material_select();
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15 // Creates a dropdown of 15 years to control year
+        });
+        $('input.autocomplete').autocomplete({
+            data: {
+                "Apple": null,
+                "Microsoft": null,
+                "Google": 'http://placehold.it/250x250'
+            }
+        });
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
