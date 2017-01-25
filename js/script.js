a0f89c400f5c624b58947e732ee0c23f8c43c49f
nicvaniek <nicvaniek@gmail.com>
2017-01-25 19:05:47 +0200

    Some change

--- js/script.js
+++ js/script.js
@@ -11,4 +11,38 @@ $(document).ready(function(){
 			$('#refCode').prop('disabled', true);
 		}
 	});
+
+	// Input validation
+	$('#regBtn').on('click', function(e){
+        if ($('#fName').val() == "" || $('#lName').val() == "" || $('#email').val() == "" || $('#pass1').val() == "" || $('#pass2').val() == "")
+        {
+        	swal({  
+                title: "Error!",   
+                text: "Please fill out all fields",   
+                type: "error",   
+                confirmButtonColor: "#DD6B55",   
+                confirmButtonText: "Close",   
+                confirmButtonColor: '#B71C1C' 
+            }); 
+
+            return false;
+        }
+        else if ($('#pass1').val() != $('#pass2').val())
+        {
+        	swal({  
+                title: "Error!",   
+                text: "Passwords don't match",   
+                type: "error",   
+                confirmButtonColor: "#DD6B55",   
+                confirmButtonText: "Close",   
+                confirmButtonColor: '#B71C1C' 
+            }); 
+
+            return false;
+        }
+        else
+        {
+        	return true;
+        }
+    });
 });
