deda65d9c3844f11b58aef93f506ecc48257483b
Nicolai van Niekerk <nicvaniek@gmail.com>
2017-02-03 13:21:35 +0200

    1028

--- php/send-recovery-email.php
+++ php/send-recovery-email.php
@@ -20,7 +20,7 @@
             $pass = $row['password'];
             $name = $row['firstName'];
 
-            $link="http://www.unhinged.co.za/Demo/Kyle1024/php/reset-password.php?key=".md5($email)."&reset=".md5($pass);
+            $link="http://www.unhinged.co.za/Demo/Kyle1028/php/reset-password.php?key=".md5($email)."&reset=".md5($pass);
 
             // Send email
             $to      = $email; // Send email to our user
